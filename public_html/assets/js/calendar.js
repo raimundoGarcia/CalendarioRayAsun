8a151fbfd5e629252a5bfaf99ff10ce1fa30d5f3
informatica4 <informatica4@informatica4-pc.consultia.dom>
2018-04-04 16:22:05 +0200

    Minicommit

--- public_html/assets/js/calendar.js
+++ public_html/assets/js/calendar.js
@@ -1088,6 +1088,8 @@ function showEventDetail(id, layout, day, month, year) {
 
         document.getElementById("descripcion").innerHTML = "";
         document.getElementById("compartecon").innerHTML = "";
+        document.getElementsByClassName("iconos")[0].innerHTML = "";
+     //   document.getElementsByClassName("iconos")[0].setAttribute('id', 'noMostrar');
 
 
 //BLOQUE COMÚN PARA CUALQUIER TIPO RESERVA
@@ -1105,15 +1107,16 @@ function showEventDetail(id, layout, day, month, year) {
         document.getElementById("verMapa").addEventListener("click", function () {
             window.open('http://maps.google.es/?q=' + coordenadas);
         });
+       //evento para "acordeón" div información clima
         document.getElementById("info-clima").addEventListener("click",function(){
           if( $(".iconos").attr("id") === "iconos"){
             $(".iconos").attr("id","noMostrar");
         }else{
           $(".iconos").attr("id","iconos");
         }
-          
+         
                
-       });
+       });  
         // Mostrar la información recomendada según el pais de destino 
 
         document.getElementById("info-lugar").addEventListener("click", function () { // esta funcion obtiene un listado de resultados con la dirección, siendo el pais la ultima 
@@ -1356,7 +1359,7 @@ function showEventDetail(id, layout, day, month, year) {
         $("#info-clima").on('click',function(){
             
                     //si la diferencia es menor de 5 días, llamar a API del clima
-                    if (diasDif <= 5){
+                    if ( (diasDif >= 0) && (diasDif <=5) ){
                         console.log("Es menor de 5 días");
                                                 
                         $.ajax({
@@ -1366,16 +1369,17 @@ function showEventDetail(id, layout, day, month, year) {
                             dataType: 'json',
                             success: function (datosClima) {
                                 console.log(datosClima);
+                                $('.iconos').append("<div>"+datosClima.list[0]+"</div>");
 
                             },
                             error: function () {
-                                alert("Se ha producido un error API u otra causa.");
+                                console.log("Se ha producido un error API u otra causa.");
                             }
                         });
                         
                       //si no, aviso al usuario 
                     }else{
-                        alert("La diferencia es mayor de 5 días (" + diasDif + "). Consulte la previsión máximo 5 días antes del inicio del viaje.");
+                        console.log("La diferencia es mayor de 5 días (" + diasDif + "). Consulte la previsión máximo 5 días antes del inicio del viaje.");
                     }       
         });
         
